c84d4bb02a05b1a85e83117721414a2ada0e34b3
Enrico <enricoadi49@gmail.com>
2023-04-03 17:37:37 +0700

    adding react router dom

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
- 
+const {useState, useContext, createContext} = React;     
+const {Link, Route, Redirect, HashRouter,BrowserRouter, Switch, useHistory} = window.ReactRouterDOM; 
+const withRouter = ReactRouterDOM.withRouter; 
 const App = ()=> {  
     return (
         <div>
-           Hello world
+            <HashRouter>
+            Link: <Link to="/register">Register</Link> | <Link to="/login">Login</Link> | <Link to="/">Home</Link>
+              
+               <Switch>
+                    <Route exact path="/register">
+                        <div> Halaman register</div>
+                    </Route>
+                    <Route exact path="/login">
+                        <div> Halaman login</div>
+                    </Route>
+                    <Route exact path="/">
+                        <div> Halaman home</div>
+                    </Route>
+                </Switch>
+            </HashRouter>  
         </div>
     )
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # react-babel-starterpack
 React with Babel 
 React Version 17
+Router version 5
